bfeee389fca946d0a480c495394bd064e9c43e95
Julian M. Bastidas P <julian.bastidas@campusucc.edu.co>
2025-02-12 08:15:55 -0500

    agregacion de models

--- api_Mascotas/mascotas/models.py
+++ api_Mascotas/mascotas/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+class Mascota(models.Model):
+    nombre = models.CharField(max_length=100)
+    peso = models.DecimalField(max_digits=5, decimal_places=2)
+    edad = models.IntegerField()
+    especie = models.CharField(max_length=100)
+    raza = models.CharField(max_length=100)
+    fecha_nacimiento = models.DateField()
+    fecha_creacion = models.DateTimeField(default=datetime.now)
+    def __str__(self):
+        return self.nombre
+    def age(self):
+        return datetime.now().year - self.fecha_nacimiento.year
