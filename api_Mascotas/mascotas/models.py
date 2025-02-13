a10f7f6adec0aca4b5ee759eb265514fb7f9449a
Julian M. Bastidas P <julian.bastidas@campusucc.edu.co>
2025-02-12 22:06:13 -0500

    cambios en models

--- api_Mascotas/mascotas/models.py
+++ api_Mascotas/mascotas/models.py
@@ -9,7 +9,7 @@ class Mascota(models.Model):
     edad = models.IntegerField()
     especie = models.CharField(max_length=100)
     raza = models.CharField(max_length=100)
-    fecha_nacimiento = models.DateField()
+    fecha_nacimiento = models.DateField(null=True, blank=True)
     fecha_creacion = models.DateTimeField(default=datetime.now)
     def __str__(self):
         return self.nombre
